3cf866ed7d4f3cda52d6761aa22e9ea49f9ca8c9
Asif Mahmud <asiffyy@gmail.com>
2019-08-08 20:19:09 +0600

    Started Signup UI Testing

---
+++ Project2/src/SignupTest.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class SignupTest {
+
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void setUp() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Projects\\Java Projects\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.navigate().to("https://www.phptravels.net/");
+	}
+
+	
+	
+	
+	@AfterMethod
+	public void shutDown() {
+		
+		driver.quit();
+	}
+
+}
