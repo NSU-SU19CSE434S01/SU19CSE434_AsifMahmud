02895f4ff502e6aa688fe4c68de86dc4f571d4de
Asif Mahmud <asiffyy@gmail.com>
2019-08-10 00:03:19 +0600

    Doing hotelBookingTest By TestNG

--- Project2/src/HotelBookingTest.java
+++ Project2/src/HotelBookingTest.java
@@ -1,9 +1,13 @@
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class HotelBookingTest {
 
@@ -16,9 +20,86 @@ public class HotelBookingTest {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 		driver.navigate().to("https://www.phptravels.net/");
+	}
+	
+	@Test
+	public void hotelBookingTest() {
+
+		//Slow Scroll
+		for (int i=0; i < 8; i++) {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,20)");
+		try {
+			Thread.sleep(40);
+			} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+		}
+		
+		//Putting in booking informations
+		driver.findElement(By.xpath("//*[@id=\"s2id_location\"]")).click();
+		driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys("Los Angeles");
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys(Keys.RETURN);
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Going date
+		driver.findElement(By.xpath("//*[@id=\"dpd1\"]/div/input")).click();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Selecting going date
+		driver.findElement(By.xpath("/html/body/div[13]/div[1]/table/tbody/tr[5]/td[6]")).click();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Selecting return date
+		driver.findElement(By.xpath("/html/body/div[14]/div[1]/table/tbody/tr[5]/td[7]")).click();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Clicking search hotel
+		driver.findElement(By.xpath("//*[@id=\"thhotels\"]/form/div[5]/button")).click();
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	
