60de9880821f5d6e19b6d2f96eafaf1635c8ac0b
Asif Mahmud <asiffyy@gmail.com>
2019-08-09 17:12:52 +0600

    Implemented signupSuccessTest By TestNG

--- Project2/src/SignupTest.java
+++ Project2/src/SignupTest.java
@@ -201,6 +201,92 @@ public class SignupTest {
 	}
 	
 	
+	@Test
+	public void signupSuccessTest() {
+		//Going into signup page
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul[2]/ul/li[1]/a")).click();
+		driver.findElement(By.xpath("/html/body/nav/div/div[2]/ul[2]/ul/li[1]/ul/li[2]/a")).click();
+		
+		//Slow Scroll
+		for (int i=0; i < 5; i++) {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,20)");
+		try {
+			Thread.sleep(40);
+			} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+		}
+
+		try {
+			Thread.sleep(2500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Sending signup inputs
+		driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[3]/input")).sendKeys("Asif");
+		driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[4]/input")).sendKeys("Mahmud");
+		driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[5]/input")).sendKeys("01671324427");
+		driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[6]/input")).sendKeys("asifmahmud@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[7]/input")).sendKeys("25051994");
+		driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[8]/input")).sendKeys("25051994");
+
+		try {
+			Thread.sleep(2500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Slow Scroll
+		for (int i=0; i < 5; i++) {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,20)");
+		try {
+			Thread.sleep(40);
+			} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+		}
+
+		//Clicking login button
+		driver.findElement(By.xpath("//*[@id=\"headersignupform\"]/div[9]/button")).click();
+
+		//Slow Scroll
+		for (int i=0; i < 10; i++) {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,-20)");
+		try {
+			Thread.sleep(40);
+			} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			}
+		}
+		
+		try {
+			Thread.sleep(2500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Checking for new user dash board title
+		boolean newUserDashboardTitle = driver.findElement(By.xpath("//*[@id=\"body-section\"]/div[1]/div/div/div[1]/h3")).isDisplayed();
+		String newUserDashboardTitleText = driver.findElement(By.xpath("//*[@id=\"body-section\"]/div[1]/div/div/div[1]/h3")).getText();
+		System.out.println(newUserDashboardTitle);
+		System.out.println(newUserDashboardTitleText);
+		
+		assertTrue(newUserDashboardTitle);
+		assertEquals(newUserDashboardTitleText,"Hi, Asif Mahmud");
+	
+	}
+
+	
 	@AfterMethod
 	public void shutDown() {
 		
