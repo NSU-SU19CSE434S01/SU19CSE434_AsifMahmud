e9e3dd7572ced9c5bddf1e14d89a4a38752ab26f
Asif Mahmud <asiffyy@gmail.com>
2019-08-12 23:23:34 +0600

    Implemented supplierSignupTest By TestNG

--- Project2/src/FooterNavigationTest.java
+++ Project2/src/FooterNavigationTest.java
@@ -280,6 +280,85 @@ public class FooterNavigationTest {
 	}
 
 
+	@Test
+	public void supplierSignupTest() throws InterruptedException {
+		
+		for (int i=0; i < 150; i++) {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,20)");
+		Thread.sleep(40);}
+		
+		Thread.sleep(2000);
+		
+		// Store the current window handle
+		String winHandleBefore = driver.getWindowHandle();
+
+		// Perform the click operation that opens new window
+		//Clicking Supplier Login
+		driver.findElement(By.xpath("/html/body/div[6]/div[2]/div[1]/div/div[4]/ul/li[1]/a")).click();
+		Thread.sleep(2000);
+
+		// Switch to new window opened
+		for(String winHandle : driver.getWindowHandles()){
+		    driver.switchTo().window(winHandle);
+		}
+		
+		// Perform the actions on new window
+		driver.findElement(By.xpath("//*[@id=\"tours\"]")).click();
+		Thread.sleep(2000);
+		
+		for (int i=0; i < 25; i++) {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,20)");
+		Thread.sleep(40);}
+		
+		Thread.sleep(2000);
+
+
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[1]/div/input")).sendKeys("Cox's Bazar Tour");
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[2]/div/input")).sendKeys("Asif");
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[3]/div/input")).sendKeys("Mahmud");
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[4]/div/input")).sendKeys("asifmahmud22@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[5]/div/input")).sendKeys("01671324427");
+		Thread.sleep(2000);
+		
+		for (int i=0; i < 22; i++) {
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("window.scrollBy(0,20)");
+		Thread.sleep(40);}
+		
+		Thread.sleep(2000);
+
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[6]/div/select")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[6]/div/select/option[10]")).click();
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[7]/div/input")).sendKeys("Chittagong");
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[8]/div/input")).sendKeys("Chittagong");
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[1]/div/fieldset/div[9]/div/input")).sendKeys("Cox's Bazar");
+		Thread.sleep(2000);
+		
+		//Register
+		driver.findElement(By.xpath("//*[@id=\"apply\"]/div/div/form/div[2]/button")).click();
+
+
+		//Wait for alert
+		WebElement alertSuccess = (new WebDriverWait(driver, 10))
+				 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"body-section\"]/div[3]/div[1]")));
+
+		boolean alertSuccessVisible = driver.findElement(By.xpath("//*[@id=\"body-section\"]/div[3]/div[1]")).isDisplayed();
+		System.out.println(alertSuccessVisible);
+		
+		//Testing
+		assertTrue(alertSuccessVisible);
+		
+		Thread.sleep(2000);
+		// Close the new window, if that window no more required
+		driver.close();
+		// Switch back to original browser (first window)
+		driver.switchTo().window(winHandleBefore);
+		
+	}
+
 
 	
 	
